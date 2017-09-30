c575d4615a18f0e96b1313670b380c01a14c6c2b
Abdul Ahmad <abdul.ahmad@gmail.com>
2017-09-30 17:27:09 -0400

    Iterate through Content. Need to: add title, image, and then create actual content components.

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import ContentTextBlock from '../ContentTextBlock/ContentTextBlock';
-// import ContentImage from '../ContentImage/ContentImage';
-// import ContentAd from '../ContentAd/ContentAd';
+import ContentTextBlock from '../ContentTextBlock/ContentTextBlock';
+import ContentImage from '../ContentImage/ContentImage';
+import ContentAd from '../ContentAd/ContentAd';
 
 const Content = ({ pageContent }) => {
-  console.log(pageContent);
-  // const contentObj = [];
-  // for (const contentSlot in pageContent) {
-  //   if (contentSlot.Type === 'TextBlock') {
-  //     contentObj.push(<ContentTextBlock content={contentSlot} />);
-  //   } else if (contentSlot.Type === 'Image') {
-  //     contentObj.push(<ContentImage content={contentSlot} />);
-  //   } else if (contentSlot.Type === 'Ad Placeholder') {
-  //     contentObj.push(<ContentAd content={contentSlot} />);
-  //   }
-  // }
-  // const contentList = contentObj.map(contentSlot =>
-  //   <div>{contentSlot}</div>,
-  // );
-  // return ({ contentList });
-  return <div />;
+  const contentObj = [];
+
+  pageContent.forEach((contentSlot) => {
+    if (contentSlot.Type === 'TextBlock') {
+      contentObj.push(<ContentTextBlock content={contentSlot} />);
+    } else if (contentSlot.Type === 'Image') {
+      contentObj.push(<ContentImage content={contentSlot} />);
+    } else if (contentSlot.Type === 'Ad Placeholder') {
+      contentObj.push(<ContentAd content={contentSlot} />);
+    }
+  });
+
+  const listItems = contentObj.map(contentSlot =>
+    <div>{contentSlot}</div>
+  );
+  return listItems;
 };
 
 export default Content;
