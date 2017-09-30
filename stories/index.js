0888c3f8e186c490372f805233045aaa86fe90ad
Abdul Ahmad <abdul.ahmad@gmail.com>
2017-09-30 19:27:54 -0400

    Added stories to storybook.

--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 
-import Ad from '../src/components/Ad/Ad';
+import Author from '../src/components/Author/Author';
+import CaptionedImage from '../src/components/CaptionedImage/CaptionedImage';
+import Content from '../src/components/Content/Content';
+import ContentAd from '../src/components/ContentAd/ContentAd';
+import ContentImage from '../src/components/ContentImage/ContentImage';
+import ContentTextBlock from '../src/components/ContentTextBlock/ContentTextBlock';
+import Page from '../src/components/Page/Page';
+import Pagination from '../src/components/Pagination/Pagination';
+import Title from '../src/components/Title/Title';
 
 // import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
@@ -15,4 +23,12 @@ import Ad from '../src/components/Ad/Ad';
 // .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
 
 storiesOf('Article', module)
-  .add('Ad', () => <Ad />);
+  .add('Author', () => <Author />)
+  .add('CaptionedImage', () => <CaptionedImage />)
+  .add('Content', () => <Content />)
+  .add('ContentAd', () => <ContentAd />)
+  .add('ContentImage', () => <ContentImage />)
+  .add('ContentTextBlock', () => <ContentTextBlock />)
+  .add('Page', () => <Page />)
+  .add('Pagination', () => <Pagination />)
+  .add('Title', () => <Title />);
