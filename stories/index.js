e48195a749c4cb5b594c259750661c9c5bc3c7b1
Abdul Ahmad <abdul.ahmad@gmail.com>
2017-09-30 20:23:04 -0400

    Added stories to storybook

--- stories/index.js
+++ stories/index.js
@@ -11,7 +11,9 @@ import Page from '../src/components/Page/Page';
 import Pagination from '../src/components/Pagination/Pagination';
 import Title from '../src/components/Title/Title';
 
-// import { action } from '@storybook/addon-actions';
+import data from '../src/data/fun-adam-sandler-facts.json';
+
+import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
 
 // import { Button, Welcome } from '@storybook/react/demo';
@@ -22,13 +24,26 @@ import Title from '../src/components/Title/Title';
 // .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
 // .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
 
+
 storiesOf('Article', module)
-  .add('Author', () => <Author />)
-  .add('CaptionedImage', () => <CaptionedImage />)
-  .add('Content', () => <Content />)
+  .add('Author', () => <Author authorBio={data.OriginalAuthor} />)
+  .add('CaptionedImage', () =>
+    <CaptionedImage
+      title={data.Title}
+      imageUrl={data.MainImageUrl}
+      caption={data.MainImageAttributions[0].SourceName}
+      captionLink={data.MainImageAttributions[0].SourceUrl}
+      alt="Here is some Alternate Text"
+    />)
+  .add('Content', () => <Content pageContent={data.Pages['1']} />)
   .add('ContentAd', () => <ContentAd />)
-  .add('ContentImage', () => <ContentImage />)
-  .add('ContentTextBlock', () => <ContentTextBlock />)
-  .add('Page', () => <Page />)
-  .add('Pagination', () => <Pagination />)
-  .add('Title', () => <Title />);
+  .add('ContentImage', () => <ContentImage content={data.Pages['1'][1]} />)
+  .add('ContentTextBlock', () => <ContentTextBlock content={data.Pages['1'][0]} />)
+  .add('Page', () => <Page data={data} />)
+  .add('Pagination', () =>
+    <Pagination
+      currentPage="2"
+      previousPage={action('previousPage')}
+      nextPage={action('nextPage')}
+    />)
+  .add('Title', () => <Title title={data.Title} />);
